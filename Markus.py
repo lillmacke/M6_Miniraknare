c1112e67452f2cbbaff210a5aa58299ad84fca2f
lillmacke <markus9karlsson@hotmail.com>
2025-10-15 09:26:02 +0200

    funktioner

---
+++ Markus.py
@@ -0,0 +1,17 @@
+import math 
+a = float(input("Första heltalet: "))
+b = float(input("Andra heltalet: "))
+
+def addition(x,y):
+    return x + y
+
+def subraktion(x,y):
+    return x - y
+
+def multiplikation(x,y):
+    return x * y
+
+def division(x,y):
+    return x / y 
+
+print(addition(a,b), "Addition")
--- main.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Första heltalet: "))
-b = int(input("Andra heltalet: "))
-
-def addition():
-    return a + b 
-
-print(addition())
