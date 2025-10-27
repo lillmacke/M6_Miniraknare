6fbdd19b2af490320e327cb3bb7279f2debbe416
williamelgen <williamelgstrom@gmail.com>
2025-10-27 21:27:52 +0100

    derivator version 2
    fixade några problem

---
+++ Derivata.py
@@ -0,0 +1,19 @@
+import sympy as sp 
+import math
+
+
+print("hej")
+choice = input("Välj räknesätt x+y*/ eller derivata: ")
+
+if  choice =='derivata':
+    x = sp.Symbol('x') #skapar en variavl som man kallar x
+    nytt_uttryck = input("Ange ett uttryck som vi ska derivera ")
+
+    uttryck_1 = sp.sympify(nytt_uttryck) #detta gör om texten till ett sympy uttryck
+    derivator = sp.diff(uttryck_1, x) #driverar uttrycket man skriver i input
+
+
+    print(f"derivatan av denna uttryck blir: {derivator}")
+
+
+
