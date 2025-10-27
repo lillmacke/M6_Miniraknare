636f3c00f8d63c522cc1410420f3a077ab2f92c1
lillmacke <markus9karlsson@hotmail.com>
2025-10-27 10:27:46 +0100

    V1 Klar

--- V1.py
+++ V1.py
@@ -12,11 +12,11 @@ def mult(x,y):
 def div(x,y):
     return x / y 
 
-print("Välj räknesätt: ")
-print("1. Addera")
-print("2. Subtract")
-print("3. Multiplicera")
-print("4. Divide")
+print("Välj räknesätt: \n"
+        "1. Addera \n"  
+        "2. Subtrahera \n"
+        "3. Multiplicera \n"
+        "4. Dividera \n")
 
 while True: 
     val = input("Välj 1/2/3/4: ")
@@ -32,17 +32,17 @@ while True:
             print(num1, "+", num2, "=", add(num1,num2))
          
         elif val == "2":
-            print(num1, "-", num2, sub(num1,num2))
+            print(num1, "-", num2, "=", sub(num1,num2))
         
         elif val == "3":
-            print(num1, "*", num2,  mult(num1,num2))
+            print(num1, "*", num2, "=",  mult(num1,num2))
         
         elif val == "4":
-            print(num1, "/", num2, div(num1,num2))
+            print(num1, "/", num2, "=", div(num1,num2))
         
-        köra_igen = input("Nästa uträkning? (ja/nej)")
+        köra_igen = input("Nästa uträkning? (ja/nej): ")
         if köra_igen == "nej":
             break
-        else:
-            print("Fel input")
+    else:
+        print("Fel input")
             
