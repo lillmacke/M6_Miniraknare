d23bf9e3dabf888855c934601709cbe5939e194f
williamelgen <williamelgstrom@gmail.com>
2025-10-27 13:24:46 +0100

    Derivator version 1
    Fixade derivator till programmet

--- Third.py
+++ Third.py
@@ -1,3 +1,19 @@
-print('hello world')
-a = 1+1
-print(a)
+import sympy as sp 
+import math
+
+
+
+choice = input("Välj räknesätt x+y*/ eller derivata: ")
+
+if  choice =='derivata':
+    x = sp.Symbol('x') #skapar en variavl som man kallar x
+    nytt_uttryck = input("Ange ett uttryck som vi ska derivera ")
+
+    uttryck_1 = sp.sympify(nytt_uttryck) #detta gör om texten till ett sympy uttryck
+    derivator = sp.diff(uttryck_1, x) #driverar uttrycket man skriver i input
+
+
+    print(f"derivatan av denna uttryck blir: {derivator}")
+
+
+
